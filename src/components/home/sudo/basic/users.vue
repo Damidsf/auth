3389055ea9dce93c8cf27a0bbd3956bdd5e2c409
mk <1115472037@qq.com>
2023-12-13 19:16:14 +0800

    添加三个接口

--- src/components/home/sudo/basic/users.vue
+++ src/components/home/sudo/basic/users.vue
@@ -2,18 +2,20 @@
   <defaultTable>
     <template #table>
       <h2>用户管理</h2>
-      <el-table :data="tableData" style="width: 100%">
-        <el-table-column fixed prop="id" label="编号" width="120" />
-        <el-table-column prop="name" label="姓名" width="200" />
-        <el-table-column prop="info" label="info" width="200" />
+      <el-table :data="currentUserList" style="width: 100%">
+        <el-table-column fixed prop="uiid" label="uuid" width="200" />
+        <el-table-column prop="username" label="用户名" width="200" />
+        <el-table-column prop="phone" label="手机号" width="200" />
+        <el-table-column prop="orgnization" label="组织" width="200" />
         <el-table-column fixed="right" label="操作" width="300">
-          <template #default>
+          <template #default="{ row }">
             <el-button link type="primary" size="small">添加</el-button>
             <el-button link type="primary" size="small">编辑</el-button>
             <el-button link type="primary" size="small">禁用</el-button>
             <el-button link type="primary" size="small">启用</el-button>
-            <el-button link type="primary" size="small">删除</el-button>
-            <el-button link type="primary" size="small">查阅</el-button>
+            <el-button link type="primary" size="small" @click="del(row)">删除</el-button>
+
+            <el-button link type="primary" size="small" @click="search(row)">查阅</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -23,32 +25,49 @@
 </template>
 <script lang='ts' setup>
 import defaultTable from '@/components/defaultTable.vue';
+import { getUserList, getUserDataByUuid, deleteUser } from "@/api/index"
+import { onBeforeMount, ref } from "vue"
+const currentUserList = ref()
+onBeforeMount(async () => {
+  await _getUserList()
+})
+const _getUserList = async () => {
+  const res = await getUserList(1, 1).catch(err => { console.warn(err); return; })
+  if (!res) return
+  if (res.data.status) {
+    console.log("获取成功");
+    currentUserList.value = res.data.data
+  } else {
+    console.log("获取失败");
+  }
+}
 
-const tableData = [
-  {
-    id: '1',
-    name: 'Tom',
-    info: 'California',
+const search = async (data: any) => {
+  const res = await getUserDataByUuid(data.uuid).catch(err => { console.warn(err); return })
+  if (!res) return
+  if (res.data.status) {
+    console.log("获取成功");
+  } else {
 
-  },
-  {
-    id: '2',
-    name: 'Tom',
-    info: 'California',
+    console.log("获取失败");
 
-  },
-  {
-    id: '3',
-    name: 'Tom',
-    info: 'California',
+  }
+}
 
-  },
-  {
-    id: '4',
-    name: 'Tom',
-    info: 'California',
+const del = async (data: any) => {
+  const res = await deleteUser(data.uuid).catch(err => { console.warn(err); return })
+  if (!res) return
+  await _getUserList()
+}
 
-  },
-]
+// const tableData = [
+//   {
+//     uuid: '1',
+//     name: 'Tom',
+//     phone: 'California',
+//     orgnization:"0"
+
+//   }
+// ]
 </script>
 <style lang='scss' scoped></style>
